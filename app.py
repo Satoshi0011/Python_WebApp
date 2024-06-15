add636fb8c0f12ab36691f3738a0f104cfff2673
baba <OCA2204240011@edu.oca.ac.jp>
2024-06-15 13:45:26 +0900

    演習

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "Response Data"
+
+
+@app.route("/another")
+def another():
+    return "Another Response"
